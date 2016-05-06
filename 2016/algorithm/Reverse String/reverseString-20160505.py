2c6c0f47028b1e2732466c19afcb5d1bd45a0415
lxieyang <lxieyang@umich.edu>
2016-05-05 22:50:46 -0400

    reverse string

---
+++ 2016/algorithm/Reverse String/reverseString-20160505.py
@@ -0,0 +1,12 @@
+
+class Solution(object):
+    def reverseString(self, s):
+        ret = ""
+        for i in range(len(s)):
+            idx = len(s) - i - 1
+            ret += s[idx]
+        return ret
+
+sol = Solution()
+concern = 'angela'
+print sol.reverseString(concern)
