706fe97be3b2280c099c07114d15e67dbed40134
lxieyang <lxieyang@umich.edu>
2016-05-11 15:46:35 -0400

    longest substring without dup

---
+++ 2016/algorithm/Longest Substring Without Repeating Characters/longsetSubString-20160511.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        globalMax = 0
+        i = 0
+        j = 0
+        posDict = dict()
+        while j < len(s) :
+            if posDict.get(s[j]) is not None:
+                i = max(i, 1 + posDict.get(s[j]))
+            globalMax = max(globalMax, j - i + 1)
+            posDict[s[j]] = j
+            j += 1
+        return globalMax
+
+
+anglea = "abcabcbbabcd"
+sol = Solution()
+print 'Length: ', sol.lengthOfLongestSubstring(anglea), "\n"
+
