87d6f7734c2b10c209ea21d11777ff27ae3f08e1
lxieyang <lxieyang@umich.edu>
2016-05-11 22:35:27 -0400

    median of two sorted array

---
+++ 2016/algorithm/Median Of Two Sorted Arrays/medianOfTwoSortedArrays-20160511.cc
@@ -0,0 +1,77 @@
+/*
+There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+*/ 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // Thoughts:
+        //      combine the two ==> [O(m + n)]
+        //      get the median
+        unsigned size = nums1.size() + nums2.size(); 
+        vector<int> total;
+        total.reserve(size);
+
+        // combine
+        unsigned i = 0, j = 0;
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] < nums2[j]) {
+                total.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i] > nums2[j]) {
+                total.push_back(nums2[j]);
+                j++;
+            }
+            else {
+                total.push_back(nums1[i]);
+                total.push_back(nums2[j]);
+                i++; j++;
+            }
+        }
+        if(i < nums1.size()) {
+            for(; i < nums1.size(); ++i) {
+                total.push_back(nums1[i]);
+            }
+        }
+        else {
+            for(; j < nums2.size(); ++j) {
+                total.push_back(nums2[j]);
+            }
+        }
+
+        // test output
+        for(auto x : total) {
+            cout << x << " ";
+        }
+        cout << "\n";
+
+        // get median
+        
+        if(size % 2 == 0) {
+            return (total[size / 2] + total[size / 2 - 1]) / 2.0;
+        }
+        else {
+            return total[size / 2];
+        }
+    }
+};
+
+
+int main() {
+    vector<int> michael = {1, 3, 5, 7};
+    vector<int> angela = {2, 4, 6, 8};
+    Solution sol;
+    double ret = sol.findMedianSortedArrays(michael, angela);
+    cout << "median: " << ret << "\n";
+}
+
+
+
