aedfd8d1b3c5dc8ffe7d164bc3cae04f2cccfb51
lxieyang <lxieyang@umich.edu>
2016-05-12 14:53:51 -0400

    median of two sorted arrays

--- 2016/algorithm/Median Of Two Sorted Arrays/medianOfTwoSortedArrays-20160511.cc
+++ 2016/algorithm/Median Of Two Sorted Arrays/medianOfTwoSortedArrays-20160511.cc
@@ -67,13 +67,48 @@ public:
 };
 
 
-// Version 2:
+// Version 2: O(log(m + n))
 class Solution2 {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         // Thoughts:
-        //      
+        //      generalization: finding the kth element in two sorted array
+        const int m = nums1.size();
+        const int n = nums2.size();
+        int total = m + n;
+        if(total >> 2 == 1) {
+            return find_kth(nums1.begin(), m, nums2.begin(), n, total / 2 + 1);
+        }
+        else {
+            return (find_kth(nums1.begin(), m, nums2.begin(), n, total / 2) + 
+                    find_kth(nums1.begin(), m, nums2.begin(), n, total / 2 + 1)) / 2.0;
+        }
         
+    }
+
+    int find_kth(vector<int>::const_iterator A, int m,
+                 vector<int>::const_iterator B, int n,
+                 int k) {
+        // assume that m <= n
+        if(m > n)
+            find_kth(B, n, A, m, k);
+        if(m == 0)
+            return *(B + k - 1);
+        if(k == 1)
+            return min(*A, *B);
+
+        // divide k into two parts
+        int ia = min(k / 2, m);
+        int ib = k - ia;
+        if(*(A + ia - 1) < *(B + ib - 1)) {
+            return find_kth(A + ia, m - ia, B, n, k - ia);
+        }
+        else if(*(A + ia - 1) > *(B + ib - 1)) {
+            return find_kth(A, m, B + ib, n - ib, k - ib);
+        }
+        else {
+            return A[ia - 1];
+        }
     }
 };
 
